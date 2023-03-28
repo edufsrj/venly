71d3783129ce237337dfe74798bf489dd90be381
Eduardo Santos <eduardo.dossantos@bosa.fgov.be>
2023-03-28 09:56:28 +0200

    task 7

--- src/main/java/com/testgorilla/venly/model/WordRelation.java
+++ src/main/java/com/testgorilla/venly/model/WordRelation.java
@@ -1,19 +1,18 @@
 package com.testgorilla.venly.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "word1", "word2" }) })
 @Builder
 @Getter
 @NoArgsConstructor
@@ -23,10 +22,7 @@ public class WordRelation {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String word1;
     private String word2;
-    @Enumerated(EnumType.STRING)
     private String type;
-
 }
